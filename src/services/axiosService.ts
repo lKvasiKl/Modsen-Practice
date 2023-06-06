9e38910d97ae382daea85cb22f4e1eee84749d32
Kirill Vasko <kirill.vasko.2003@gmail.com>
2023-06-06 21:48:35 +0300

    Add axios service

---
+++ src/services/axiosService.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:9000',
+});
+
+interface RequestParams {
+    headers?: Record<string, string>;
+    method: 'POST' | 'GET';
+    url: string;
+    data?: any;
+    params?: Record<string, string>;
+}
+
+const request = async ({ headers = {}, method, url, data, params }: RequestParams) => {
+    const options: AxiosRequestConfig = {
+        headers,
+        method,
+        url,
+        data,
+        params,
+    };
+
+    try {
+        const result = await axiosInstance(options);
+
+        return result.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export { request };
