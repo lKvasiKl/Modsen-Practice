2c42878a015241ebf67c353d154c732bd81cafb3
Kirill Vasko <kirill.vasko.2003@gmail.com>
2023-06-22 16:08:35 +0300

    fix: eslint errors

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-useless-catch */
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -10,29 +9,17 @@ import { IAuthProps } from "shared/types";
 import { addSessionToCookie } from "helpers/tokens";
 
 const register = async ({ email, password }: IAuthProps) => {
-  try {
-    const auth = getAuth();
-    const { user } = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password,
-    );
-    const accessToken = await user.getIdToken();
-    addSessionToCookie("accessToken", accessToken);
-  } catch (error) {
-    throw error;
-  }
+  const auth = getAuth();
+  const { user } = await createUserWithEmailAndPassword(auth, email, password);
+  const accessToken = await user.getIdToken();
+  addSessionToCookie("accessToken", accessToken);
 };
 
 const login = async ({ email, password }: IAuthProps) => {
-  try {
-    const auth = getAuth();
-    const { user } = await signInWithEmailAndPassword(auth, email, password);
-    const accessToken = await user.getIdToken();
-    addSessionToCookie("accessToken", accessToken);
-  } catch (error) {
-    throw error;
-  }
+  const auth = getAuth();
+  const { user } = await signInWithEmailAndPassword(auth, email, password);
+  const accessToken = await user.getIdToken();
+  addSessionToCookie("accessToken", accessToken);
 };
 
 export { register, login };
